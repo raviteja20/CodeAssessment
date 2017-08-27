85e00987119e5f9572568b7c340931579c8f2535
teja@predictronics <teja@predictronics.com>
2017-08-27 12:01:26 -0400

    updating solution

--- PS-get-deep-property/DeepCopySolution.js
+++ PS-get-deep-property/DeepCopySolution.js
@@ -1,18 +1,19 @@
-getInputs();
-function getInputs(){
-	const obj = {person:{name:{first: 'FirstName', middleInitial: 'I',lastName: 'LastName'}}};
-	const path = 'person.name.lastName';
-	if (obj && path)
-		deepCopy(obj,path);
-	else
-		console.log("provide object and path details in the same order");
-}
+//Hard code object and path in the variables below
+const obj = {person:{name:{first: 'FirstName', middleInitial: 'I',lastName: 'LastName'}}};
+const path = 'person.name.lastName';
+
+deepCopy(obj,path);
 
 function deepCopy(obj,path){
 	try{
-		let pathElements = path.split('.');
-		let final = pathElements.reduce((currentObj,element)=>currentObj[element],obj);
-		console.log("The value of the destination property is:"+final);		
+		if (obj && path)
+		{
+			let pathElements = path.split('.');
+			let final = pathElements.reduce((currentObj,element)=>currentObj[element],obj);
+			console.log("The value of the destination property is:"+final);
+		}	
+		else
+			console.log("provide object and path details in the same order");	
 	}
 	catch(err){
 		console.log("Please provide the proper object and relevant path.Error msg:"+err);
